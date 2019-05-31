a9ffb94933382456c5a0455fa67e6a59ee9a6b5a
丨Rainbow丨 <775183940@qq.com>
2019-05-31 14:04:55 +0800

    update: value animator finish

--- sample/src/main/java/com/hyfun/animation/ui/property/ValueAnimationActivity.java
+++ sample/src/main/java/com/hyfun/animation/ui/property/ValueAnimationActivity.java
@@ -1,5 +1,7 @@
 package com.hyfun.animation.ui.property;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
 import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.view.View;
@@ -9,12 +11,6 @@ import com.hyfun.animation.base.BaseActivity;
 
 public class ValueAnimationActivity extends BaseActivity {
 
-    ValueAnimator valueAnimator = null;
-    private int currentWidth = 400;
-
-    private View propertyValueAnimationOfIntXml;
-    private View propertyValueAnimationOfIntCode;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +18,18 @@ public class ValueAnimationActivity extends BaseActivity {
 
         setDisplayHomeAsUpEnabled();
 
-        propertyValueAnimationOfIntXml = findViewById(R.id.property_value_animation_ofint_xml);
-        propertyValueAnimationOfIntCode = findViewById(R.id.property_value_animation_ofint_code);
-
-        // 先设置code的动画
+        //         <!--ValueAnimator.ofInt(int values)-->
         {
+            View propertyValueAnimationOfIntXml = findViewById(R.id.property_value_animation_ofint_xml);
+            final View propertyValueAnimationOfIntCode = findViewById(R.id.property_value_animation_ofint_code);
+            // xml
+            // 载入XML动画
+            Animator animator = AnimatorInflater.loadAnimator(this, R.animator.property_value_animation_ofint);
+            // 设置动画对象
+            animator.setTarget(propertyValueAnimationOfIntXml);
+            // 启动动画
+            animator.start();
+            // code
             ValueAnimator valueAnimator = ValueAnimator.ofInt(propertyValueAnimationOfIntCode.getLayoutParams().width, 1000);
             valueAnimator.setDuration(1000);
             valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
@@ -36,7 +39,7 @@ public class ValueAnimationActivity extends BaseActivity {
                 public void onAnimationUpdate(ValueAnimator animation) {
                     int currentValue = (Integer) animation.getAnimatedValue();
                     // 获得每次变化后的属性值
-                    System.out.println(currentValue);
+                    //System.out.println(currentValue);
                     // 输出每次变化后的属性值进行查看
                     propertyValueAnimationOfIntCode.getLayoutParams().width = currentValue;
                     // 每次值变化时，将值手动赋值给对象的属性
@@ -46,6 +49,44 @@ public class ValueAnimationActivity extends BaseActivity {
                 }
             });
             valueAnimator.start();
+        }
+
+        //         <!--ValueAnimator.oFloat（float values）-->
+        {
+            View propertyValueAnimationOfFloatXml = findViewById(R.id.property_value_animation_offloat_xml);
+            final View propertyValueAnimationOfFloatCode = findViewById(R.id.property_value_animation_offloat_code);
+            // xml
+            // 载入XML动画
+            Animator animator = AnimatorInflater.loadAnimator(this, R.animator.property_value_animation_offloat);
+            // 设置动画对象
+            animator.setTarget(propertyValueAnimationOfFloatXml);
+            // 启动动画
+            animator.start();
+            // code
+            ValueAnimator valueAnimator = ValueAnimator.ofFloat(propertyValueAnimationOfFloatCode.getLayoutParams().width, 1000);
+            valueAnimator.setDuration(1000);
+            valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
+            valueAnimator.setRepeatMode(ValueAnimator.REVERSE);
+            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    float currentValue = (Float) animation.getAnimatedValue();
+                    // 获得每次变化后的属性值
+                    //System.out.println(currentValue);
+                    // 输出每次变化后的属性值进行查看
+                    propertyValueAnimationOfFloatCode.getLayoutParams().width = (int) currentValue;
+                    // 每次值变化时，将值手动赋值给对象的属性
+                    // 即将每次变化后的值 赋 给按钮的宽度，这样就实现了按钮宽度属性的动态变化
+                    // 步骤4：刷新视图，即重新绘制，从而实现动画效果
+                    propertyValueAnimationOfFloatCode.requestLayout();
+                }
+            });
+            valueAnimator.start();
+        }
+
+
+        //         <!--ValueAnimator.ofObject()-->
+        {
         }
 
     }
